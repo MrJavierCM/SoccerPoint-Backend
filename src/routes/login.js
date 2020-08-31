ca550bcea427092812174ab0317f598484d14a28
MrJavierCM <jacamia@inf.upv.es>
2020-08-31 11:01:39 +0200

    login en correcto funcionamiento

--- src/routes/login.js
+++ src/routes/login.js
@@ -2,13 +2,57 @@ const { Router } = require('express');
 const router = Router();
 const firebase = require('firebase');
 
-router.get('/api/login', (req, rep)=>{
-    firebase.auth().singInWithEmailAndPassword(req.body.email, req.body.password).catch(function(error){
+var isClient = false;
+var isBar = false;
+ 
+router.post('/api/signOut'), (req, rep) => {
+    firebase.auth().signOut().then(function(){
+        console.log("Sign out")
+        rep.send(true)
+    }, function(error){
+        console.log(error)
+        rep.send(error)
+    });
+}
+
+router.post('/api/loginApp', (req, rep)=>{
+    firebase.auth().signInWithEmailAndPassword(req.body.Email, req.body.Password)
+    .then(() => {
+        getUserType(req.body.Email).then(() => {
+            if (isClient){
+                rep.send(200, 0);
+            } else if (isBar){
+                rep.send(200, 1);
+            }
+        });
+    })
+    .catch(function(error){
         var errorCode = error.code;
         var errorMessage = error.message;
-        rep.send(errorCode, errorMessage);
+        rep.status(403).send(errorMessage);
     })
-    rep.send(true);
 })
+
+async function getUserType(email){
+    var bar;
+
+    await firebase.database().ref('pubs').orderByChild('Email').equalTo(email).once('value').then((snapshot) => {
+        bar = snapshot.val();
+    });
+
+    if (bar != null) {
+        isBar = true
+    } else {
+        var client;
+
+        await firebase.database().ref('users').orderByChild('Email').equalTo(email).once('value').then((snapshot) => {
+            client = snapshot.val();
+        })
+
+        if( client != null){
+            isClient = true
+        }
+    }
+}
 
 module.exports = router;
