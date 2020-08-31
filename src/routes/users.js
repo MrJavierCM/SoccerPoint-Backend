875505a0db2bcb1cdb82b42ff5f94e04f1355ae8
MrJavierCM <jacamia@inf.upv.es>
2020-08-31 11:02:47 +0200

    Obtener usuario por email

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,15 +3,15 @@ const router = Router();
 const firebase = require('firebase');
 
 router.get('/api/users', (req, res) => {
-    var reference = firebase.database().ref('/users');
-    reference.on("value", function(snapshot){
-        res.json(snapshot.val())
-        reference.off("value")
-     },
-      function(errorObject){
-       res.send("The read failed: " + errorObject.code);
-     })
-   });
+  var reference = firebase.database().ref('/users');
+  reference.on("value", function(snapshot){
+    res.json(snapshot.val())
+      reference.off("value")
+  },
+  function(errorObject){
+    res.send("The read failed: " + errorObject.code);
+  })
+});
 
 router.post('/api/new-user', (req, res)=>{
   var email = req.body[0].Email;
@@ -28,19 +28,6 @@ router.post('/api/new-user', (req, res)=>{
   })
 });
 
-
-router.post('/api/login', (req,res)=>{
-  var email = req.body.Email;
-  var password = req.body.Password;
-  firebase.auth().signInWithEmailAndPassword(email, password)
-  .then(function(){
-    res.send(true)
-  })
-  .catch(function(){
-    res.send(false)
-  })
-})
-
 router.post('/api/infoUser', (req, res)=>{
   var reference = firebase.database().ref('users');
   reference.orderByChild("Nickname").equalTo(req.body.Nickname).on('value',function(snapshot){
@@ -54,6 +41,15 @@ router.post('/api/new-comment', (req, res)=>{
   reference.child(req.body[0]).child('Comments').child(req.body[1].User).set(req.body[1])
   .then(function(){
     res.send(true);
+  });
+})
+
+router.post('/api/userByEmail', (req, res) => {
+  console.log(req.body)
+  var reference = firebase.database().ref('users');
+  reference.orderByChild("Email").equalTo(req.body.Email).on('value',function(snapshot){
+      res.send(snapshot.val());
+      reference.off("value")
   });
 })
 
