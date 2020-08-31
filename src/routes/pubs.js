631ad4d6c3dc320e0c286dfdbba2aaa9e351b79d
MrJavierCM <jacamia@inf.upv.es>
2020-08-31 11:02:22 +0200

    Añadidas funcionalidades de comentarios y menú

--- src/routes/pubs.js
+++ src/routes/pubs.js
@@ -14,6 +14,7 @@ router.get('/api/pubs', (req, res) => {
 });
 
 router.post('/api/new-pub', (req, res)=>{
+  console.log(req.body)
   var email = req.body[0].Email;
   var nick = req.body[0].Nickname;
   var password = req.body[1];
@@ -51,14 +52,12 @@ router.post('/api/pubsByNick', (req, res) => {
   }); 
 })
 
-router.post('/api/pubsByEmail', (req, res) => {
+router.post('/api/pubByEmail', (req, res) => {
+  console.log(req.body)
   const pubs = [];
   var reference = firebase.database().ref('pubs');
   reference.orderByChild("Email").equalTo(req.body.Email).on('value',function(snapshot){
-      snapshot.forEach(childSnapshot => {
-        pubs.push(childSnapshot.val())
-      })
-      res.send(pubs);
+      res.send(snapshot.val());
       reference.off("value")
   });
 })
@@ -70,14 +69,35 @@ router.post('/api/editProfile', (req, res) => {
 })
 
 router.post('/api/commentsByPub', (req, res) => {
-  console.log(req.body.Nickname)
+  var nick = req.body.Nickname;
   var reference = firebase.database().ref('pubs');
-  reference.orderByChild('Nickname').equalTo(req.body.Nickname).on('value',function(snapshot){
+  reference.orderByChild('Nickname').equalTo(nick).on('value',function(snapshot){
     valueJSON = snapshot.toJSON()
-    console.log(valueJSON)
-    res.send(valueJSON.Prueba1.Comments);
+    res.send(valueJSON[nick].Comments);
     reference.off("value")
   })
 })
 
+router.post('/api/uploadImages', (req, res) => {
+  var reference = firebase.storage().ref()
+})
+
+router.post('/api/addDish', (req, res) => {
+  var reference = firebase.database().ref('pubs');
+  reference.child(req.body[0]).child('Menu').child(req.body[1].Name).set(req.body[1])
+  .then(function(){
+    res.send(true);
+  });
+})
+
+router.post('/api/menu', (req, res) => {
+  var nick = req.body.Nickname;
+  var reference = firebase.database().ref('pubs');
+  reference.orderByChild('Nickname').equalTo(nick).on('value',function(snapshot){
+    valueJSON = snapshot.toJSON()
+    res.send(valueJSON[nick].Menu);
+    reference.off("value")
+  })
+})
+ 
 module.exports = router;
